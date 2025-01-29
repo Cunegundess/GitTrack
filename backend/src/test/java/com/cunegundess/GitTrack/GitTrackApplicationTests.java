a6b686bd633338b9e182cf3c9457fd6cb1a24d95
Lucas Cunegundes <lucascsantana6@gmail.com>
2025-01-29 00:18:47 -0300

    feat: estrutura inicial do projeto com docker

---
+++ backend/src/test/java/com/cunegundess/GitTrack/GitTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunegundess.GitTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
