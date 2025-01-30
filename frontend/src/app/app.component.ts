a161227673cc95b1ae9553b47b942c5f5640e054
Lucas Cunegundes <lucascsantana6@gmail.com>
2025-01-30 12:59:29 -0300

    feat(frontend): Implementando o design e deletando o componente dashboard

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,40 @@
+import { DatePipe, NgForOf, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { FormsModule } from '@angular/forms';
+import { GithubService } from './service/github.service';
 
 @Component({
   selector: 'app-root',
-  imports: [DashboardComponent],
+  imports: [
+    FormsModule,
+    DatePipe,
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'GitTrack';
+
+  username: string = '';
+  events: any[] = [];
+  loading: boolean = false;
+  error: string | null = null;
+
+  constructor(private githubService: GithubService) {}
+
+  async fetchEvents() {
+    if (!this.username) return;
+    this.loading = true;
+    this.error = null;
+
+    try {
+      this.events = await this.githubService.getUserEvents(this.username);
+    } catch (error) {
+      this.error = "Erro ao buscar eventos do usuário";
+    } finally {
+      this.loading = false;
+    }
+  }
 }
